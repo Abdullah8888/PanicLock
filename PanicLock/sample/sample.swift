b6e3e0b5e0b28d4e2ffa8df3ccba7d0cea43565d
Abdullah8888 <abdullahijimoh3.ja@gmail.com>
2020-12-26 00:50:51 +0100

    Added UI for create savepin page

---
+++ PanicLock/sample/sample.swift
@@ -0,0 +1,30 @@
+//
+//  sample.swift
+//  PanicLock
+//
+//  Created by Jimoh Babatunde Olalekan on 25/12/2020.
+//  Copyright © 2020 Jimoh Babatunde Olalekan. All rights reserved.
+//
+
+import UIKit
+
+class sample: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
